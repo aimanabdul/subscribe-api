553eb6d169d073f560c4dd3ba1d2bc6deff30c46
aiman abdulsalam <aiman.abdulsalam.mg@gmail.com>
2025-06-12 14:04:06 +0200

    cors header toegevoegd

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SubscribeController;
+use App\Http\Middleware\Cors;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 
-Route::apiResource('subscribe', SubscribeController::class);
+Route::apiResource('subscribe', SubscribeController::class)->middleware(Cors::class);
